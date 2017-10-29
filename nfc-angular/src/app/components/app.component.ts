ee83eec40c3a003b76cddb15891ae653b639da66
aribrenman@hotmail.com <aribrenman@hotmail.com>
2017-10-28 21:46:15 -0300

    Smart table con 2 errores

--- nfc-angular/src/app/components/app.component.ts
+++ nfc-angular/src/app/components/app.component.ts
@@ -1,36 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ApiService } from '../shared/api.service';
+import { LocalDataSource } from 'ng2-smart-table';
 
 import '../../style/app.scss';
 
 @Component({
   selector: 'my-app', // <my-app></my-app>,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+ // templateUrl: './app.component.html',
+ template: '<input #search class="search" type="text" placeholder="Search..." (keydown.enter)="onSearch(search.value)"> <ng2-smart-table [settings]="settings" [source]="source"></ng2-smart-table>',
+ styleUrls: ['./app.component.scss'],
+  
+  
 })
 
 export class AppComponent implements OnInit {
-  
-   	private data : any;
 
-  	constructor(private api: ApiService) { }
+	settings = {
+		columns: {		 
+		  name: {
+			title: 'name',
+			filter: false
+		  },
+		  lastName: {
+			title: 'lastName',
+			filter: false
+		  },
+		  expedient: {
+			title: 'expedient',
+			filter: false
+		  },
+		  nfcTag:{
+			title: 'nfcTag',
+			filter: false  
+		  }
+
+		}
+	  };
+   	private data : any;
+	source: LocalDataSource;
+  	
 
 	ngOnInit() {
 		 this.api.getEmployees().subscribe(
 		      data => {
-		          this.data = data;
+				  this.data = data;
+				 
 		          console.log('EMPLOYEE FOUND: ', this.data);
 		      }
 		);
+		
 	}
-
-	remove(employee: any) {
+	constructor(private api: ApiService) { this.source = new LocalDataSource(this.data); }
+	  onSearch(query: string = '') {
+		this.source.setFilter([
+		  // fields we want to include in the search
+		  {
+			field: 'name',
+			search: query
+		  },
+		  {
+			field: 'lastName',
+			search: query
+		  },
+		  {
+			field: 'expedient',
+			search: query
+		  },
+		  {
+			field: 'nfcTag',
+			search: query
+		  }
+		], false); 
+		// second parameter specifying whether to perform 'AND' or 'OR' search 
+		// (meaning all columns should contain search query or at least one)
+		// 'AND' by default, so changing to 'OR' by setting false here
+	  }
+	/*remove(employee: any) {
       // this.data = _.filter(this.data, (elem)=>elem!=employee);
       console.log("Remove employee ID: ", employee._id);
     }
 
     edit(employee: any){
     	console.log("Edit employee ID: ", employee._id);
-    }
+    }*/
 }
